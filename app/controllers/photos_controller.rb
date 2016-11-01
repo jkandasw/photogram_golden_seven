a592a50936b7fda452c0e9a2472e4b2d7cc4c9bc
JayaS <JayaS@users.noreply.github.com>
2016-11-01 11:17:37 -0500

    Changes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,4 +9,24 @@ end
 def new_form
   @photo=Photo.find_by({:id=> params[:id]})
 end
+
+def create_row
+  p=Photo.new
+  p.caption=params[:the_caption]
+    p.source=params[:the_source]
+    p.save
+    redirect_to("http://localhost:3000/photos")
+end
+
+def destroy
+  p=Photo.find_by({:id=> params[:id]})
+  p.destroy
+  # <h2> "Photo has been deleted. You will be redirected to the home page"
+  redirect_to("http://localhost:3000/photos")
+end
+
+def edit_form
+  @photo=Photo.find_by({:id=> params[:id]})
+render("edit_form.html.erb")
+end
 end
